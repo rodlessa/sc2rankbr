0e20f71d64081fddc8fddd5310c0c7a4063f01d9
Rodrigo Lessa <rodlessa@users.noreply.github.com>
2018-01-19 22:17:15 -0300

    Comentários sobre a ladder id e charid

--- index.php
+++ index.php
@@ -5,13 +5,11 @@ include "runmmr.php"
 
 
 <div class="container">
-<input type="text" name="IDC" value="">
-<input type="text" name="IDL" value="">
 <?php
 
-$id = "862303";
-$ladderId = "262705";
-
+$id = "862303"; #isso é pego a aprtir de uma url da blizzard no caso http://us.battle.net/sc2/en/profile/862303/2/Kyun/
+$ladderId = "262705"; #isso é pego na mesma url porém da ladder http://us.battle.net/sc2/en/profile/862303/2/Kyun/ladder/262705#current-rank
+# o caminho feito para isso é ir no perfil>Ladders>Current Season
 runMMR("262705","862303");
 
  ?>
