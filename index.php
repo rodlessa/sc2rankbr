7e38e0ffbde2e228815a410f82c6466d3bc2b8d7
Rodrigo Lessa <rodlessa@users.noreply.github.com>
2018-01-20 11:52:34 -0300

    Update
    Update with JS function to sort the MMR

--- index.php
+++ index.php
@@ -1,21 +1,52 @@
 <?php
 include "header.php";
-include "runmmr.php"
+include "runmmr.php";
+include "conn.php";
 ?>
-
-
 <div class="container">
+<table id="myTable" class="table table-dark">
+  <thead>
+  <tr>
+     <th>Name</th>
+     <th>score</th>
+   </tr>
+   </thead>
 <?php
-
-$id = "862303"; #isso é pego a aprtir de uma url da blizzard no caso http://us.battle.net/sc2/en/profile/862303/2/Kyun/
-$ladderId = "262705"; #isso é pego na mesma url porém da ladder http://us.battle.net/sc2/en/profile/862303/2/Kyun/ladder/262705#current-rank
-# o caminho feito para isso é ir no perfil>Ladders>Current Season
-runMMR("262705","862303");
-
+$sql = "SELECT ladderid, charid FROM ranks";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        $ladderId = $row["ladderid"];
+        $charid = $row["charid"];
+        runMMR($ladderId,$charid);
+    }
+}
  ?>
-</div>
+ </table>
 
+</div>
+<script>
+    function sortTable() {
+      var table = document.getElementById("myTable");;
+      var rows = table.getElementsByTagName("TR");
+      var currentRow;
+      var nextRow;
+
+      for (var i = 1; i < rows.length - 1; i++) {
+        for (var j = 1; j < rows.length - 1; j++) {
+          currentRow = rows[j].getElementsByTagName("TD")[1];
+          nextRow = rows[j + 1].getElementsByTagName("TD")[1];
+          if (Number(currentRow.innerHTML) < Number(nextRow.innerHTML)) {
+            rows[j].parentNode.insertBefore(rows[j + 1], rows[j]);
+          }
+        }
+      }
+    }
+     sortTable();
+  </script>
 
 <?php
+$conn->close();
 include "footer.php";
  ?>
