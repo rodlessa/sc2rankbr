f10a5f8bec604cb4429c307606ec2041a8573ff7
Rodrigo Lessa <rodlessa@users.noreply.github.com>
2018-01-20 11:50:32 -0300

    Conexão e Inserção

---
+++ insert.php
@@ -0,0 +1,21 @@
+<?php
+include 'header.php';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "rank";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+$char_id = mysqli_real_escape_string($conn, $_REQUEST['char_id']);
+$ladder_id = mysqli_real_escape_string($conn, $_REQUEST['ladder_id']);
+
+$sql = "INSERT INTO ranks (charid, ladderid) VALUES ('$char_id', '$ladder_id')";
+if(mysqli_query($conn, $sql)){
+    echo "Cadatro feito, você será redirecionado para pagina principal em 10seg.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+}
+header('refresh: 10; url=index.php');
+mysqli_close($conn);
+?>
