edf26e2486846bf335d1d8af39dc8134a5b0e80f
Rodrigo Lessa <rodlessa@users.noreply.github.com>
2018-01-20 21:20:26 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,28 @@
+<?php include "header.php";
+require('Client.php');
+require('GrantType/IGrantType.php');
+require('GrantType/AuthorizationCode.php');
+
+$client_id		= '';
+$client_secret		= '';
+$region			= '';
+$locale			= '';
+$redirect_uri		= '';
+?>
+
+<div class="container">
+  <form action="insert.php" method="post">
+    <div class="form-group">
+        <p class="">
+          <label class="form-control" for="char_id">Char ID:</label>
+          <input class="form-control" type="text" name="char_id" id="char_id">
+        </p>
+        <p>
+          <label class="form-control" for="ladder_id">Ladder ID:</label>
+          <input class="form-control" type="text" name="ladder_id" id="ladder_id">
+      </p>
+      <input class="btn btn-primary" type="submit" value="Submit">
+  </form>
+</div>
+</div>
+<?php include "footer.php" ?>
