71c6b7fa951fb80f9eddd39673fbc623fc33f507
Rodrigo Lessa <rodlessa@users.noreply.github.com>
2018-01-23 13:44:34 -0300

    Update index.php
    Update da função de sorteio para exibir as posições em números.

--- index.php
+++ index.php
@@ -2,23 +2,24 @@
 include "header.php";
 include "runmmr.php";
 include "conn.php";
+// Configurações
 ?>
-
 <div class="container">
   <div class="alert alert-primary" role="alert">
     Para saber como se cadastrar veja o <a href="tutorial.html">tutorial</a>.
   </div>
-<table id="myTable" class="table table-dark">
+<table id="myTable" class="table table-dark myTable">
   <thead>
   <tr>
+    <th scope="row">Posição</th>
      <th scope="row">Name</th>
-     <th scope="row">score</th>
+     <th scope="row">MMR</th>
    </tr>
   </thead>
   <tbody>
 
-
 <?php
+# start function to read players
 $sql = "SELECT ladderid, charid FROM ranks";
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
@@ -32,28 +33,30 @@ if (mysqli_num_rows($result) > 0) {
  ?>
  </tbody>
  </table>
-
 </div>
 <script>
+// sort from higher mmr to lower
     function sortTable() {
       var table = document.getElementById("myTable");;
       var rows = table.getElementsByTagName("TR");
       var currentRow;
       var nextRow;
-
       for (var i = 1; i < rows.length - 1; i++) {
         for (var j = 1; j < rows.length - 1; j++) {
-          currentRow = rows[j].getElementsByTagName("TD")[1];
-          nextRow = rows[j + 1].getElementsByTagName("TD")[1];
+          currentRow = rows[j].getElementsByTagName("TD")[2];
+          nextRow = rows[j + 1].getElementsByTagName("TD")[2];
           if (Number(currentRow.innerHTML) < Number(nextRow.innerHTML)) {
             rows[j].parentNode.insertBefore(rows[j + 1], rows[j]);
+            }
           }
         }
+        text = 'textContent' in document ? 'textContent' : 'innerText';
+        for (var i = 1, len = rows.length; i < len; i++){
+        rows[i].children[0][text] = i + '. ' + rows[i].children[0][text];
       }
     }
     sortTable();
   </script>
-
 <?php
 $conn->close();
 include "footer.php";
