a7e9af05307763394ada3f49fc967fa77f4b8433
Rodrigo Lessa <rodlessa@users.noreply.github.com>
2018-01-23 13:45:11 -0300

    Update runmmr.php
    Update para adicionar a classe de posições ser exibido no php.

--- runmmr.php
+++ runmmr.php
@@ -1,6 +1,7 @@
-<?php function runMMR($ladderId,$id)
-{
+<?php
 
+
+function runMMR($ladderId,$id){
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -12,18 +13,20 @@
   $team_count = count($team);
   for ($i=0;$i<$team_count;$i++){
     $checkid = $obj->team[$i]->member[0]->legacy_link->id;
-    if ($checkid != $id) {
-
-    }else{
+    if ($checkid == $id) {
       $name = $obj->team[$i]->member[0]->legacy_link->name;
       $race = $obj->team[$i]->member[0]->played_race_count[0]->race;
       $mmr = $obj->team[$i]->rating;
       echo '<tr class="user">';
+      echo '<td class=posicao>'.'</td>';
       echo '<td class="user_name">'.$name." - ".$race.'</td>';
       echo '<td class="user_mmr">'.$mmr.'</td>';
       echo "</tr>";
-    }
+    }else{
 
     }
+
   }
+
+}
 ?>
