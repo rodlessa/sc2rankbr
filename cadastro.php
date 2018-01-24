9d3bf2356ba4227941dfab4ddd9e1cc2db835dd1
Rodrigo Lessa <rodlessa@users.noreply.github.com>
2018-01-24 18:03:39 -0300

    Update cadastro.php
    Cadastro simplificado, parte do código antigo retirado.

--- cadastro.php
+++ cadastro.php
@@ -1,17 +1,15 @@
-<?php include "header.php"; ?>
+<?php include "header.php";?>
+
 <div class="container">
   <form action="insert.php" method="post">
     <div class="form-group">
-        <p class="">
-          <label class="form-control" for="char_id">Char ID:</label>
-          <input class="form-control" type="text" name="char_id" id="char_id">
-        </p>
-        <p>
-          <label class="form-control" for="ladder_id">Ladder ID:</label>
-          <input class="form-control" type="text" name="ladder_id" id="ladder_id">
+      <p class="">
+        <label class="form-control" for="url_cad">URL do perfil</label>
+        <input class="form-control" type="text" name="url_cad" id="url_cad">
       </p>
       <input class="btn btn-primary" type="submit" value="Submit">
   </form>
 </div>
 </div>
 <?php include "footer.php" ?>
+
