e3088fcf9538725e21fe7492dab7c5aa6b9fcc23
Rodrigo Lessa <rodlessa@users.noreply.github.com>
2018-01-24 18:03:12 -0300

    Update insert.php
    Codigo do @mahousenhi adicionado simplificando o cadastro.

--- insert.php
+++ insert.php
@@ -6,22 +6,41 @@ $password = "";
 $dbname = "rank";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-$char_id = mysqli_real_escape_string($conn, $_REQUEST['char_id']);
-$ladder_id = mysqli_real_escape_string($conn, $_REQUEST['ladder_id']);
 
-$sql = "INSERT INTO ranks (charid, ladderid) VALUES ('$char_id', '$ladder_id')";
-if(mysqli_query($conn, $sql)){
-    echo "Cadatro feito, você será redirecionado para pagina principal em 10seg.";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+$url_char = $_POST["url_cad"];
+// função feita pelo @mahousenhi!
+function getDataFromUrl($url) {
+    $arr = explode('/', $url);
+    $pos = strpos($arr[10], '#');
+
+    if ($pos) {
+        $arr[10] = substr($arr[10], 0, $pos);
+    }
+
+    return array($arr[6], $arr[8], $arr[10]);
 }
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+list($char_id, $nick, $ladder_id) = getDataFromUrl($url_char);
+$sqlurl = "INSERT INTO ranks (charid, ladderid) VALUES('$char_id','$ladder_id')";
+if (mysqli_query($conn, $sqlurl)) {
+  echo "<div class= container><div class='alert alert-success' role='alert'>";
+  echo "Cadastro de URL feito</div>";
+  echo "</div>";
+}
+else {
+  echo "<div class= container><div class='alert alert-danger' role='alert'>";
+  echo "Não foi possivel ser feito o cadastro.</div>";
+  echo "</div>";
+}
+
 $remove = "DELETE FROM ranks WHERE ladderid =0";
 if(mysqli_query($conn, $remove)){
 
 } else{
 
 }
-header('refresh: 10; url=index.php');
+header('refresh: 3; url=index.php');
 mysqli_close($conn);
 ?>
